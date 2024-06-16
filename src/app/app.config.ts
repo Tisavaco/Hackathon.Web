c59264ffe01ca3df9ef6ac38f37fdc430ac937d6
Sergey <Alexandrov@2units.ru>
2024-06-16 12:16:23 +0300

    Initial project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,3 +6,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
+
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://87.242.103.80'
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { LayoutComponent } from './layout';
+
+import { ModulesInfo } from './modules';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/search',
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'search',
+                loadChildren: () => import('./modules/search/search.module').then(module => module.SearchModule),
+                data: { module: ModulesInfo.search.name }
+            }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
